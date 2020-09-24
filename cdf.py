b6c14d593a186410f9a607bfe9936bfe4a4e6978
SorayaMitate <mitate0712@gmail.com>
2020-09-24 16:35:42 +0900

    modified cdf.py

--- cdf.py
+++ cdf.py
@@ -40,12 +40,12 @@ def iroiro(s):
 
 #40ノードの正解データ
 path = 'C:\\Users\\soraya-PC\\code\\Simulation_NN\\'
-'''
 s = path + '40' + '_error.csv'
 data = pd.read_csv(s,index_col=0)
 col = data.columns
 print('--- proposed ---  ', col[1])
-s1, cdf1 = test(list(data['enn']))
+s40, cdf40 = test(list(data['enn']))
+'''
 iroiro(s1)
 plt.scatter(s1, cdf1, s=10,label='True value')
 '''
@@ -76,22 +76,55 @@ for n in l_name:
     '''
     #plt.scatter(s0, cdf0, s=10,label='RSSIExp. by Only IDW')
     #plt.scatter(s1, cdf1, s=10,label='RSSIExp. by Only NN')
-    plt.scatter(s1, cdf1, s=10,label=str(n))
+    #plt.scatter(s1, cdf1, s=10,label=str(n))
 
-plt.legend(loc='upper left',fontsize=20)
-plt.grid()
-plt.xlabel('Expected Error [dB]',fontname="HGGothicM",fontsize=30)
-plt.ylabel('CDF',fontname="HGGothicM",fontsize=30)
-plt.show()
 
-points = tuple(l_points)
-name = tuple(l_name)
+l_name.append('no interfered')
+l_points.append(s40)
+
+for i in range(len(l_points)):
+    if i == 0:
+        leng = len(l_points[i])
+    elif leng > len(l_points[i]):
+        leng = len(l_points[i])
+        
+for i in range(len(l_points)):
+    if leng > len(l_points[i]):
+        l_points[i] = np.array(l_points[i])
+        np.append(l_points[i], np.array([np.nan]*(leng-len(l_points[i]))))
+    else:
+        pass
+
+for i in range(len(l_points)):
+    if i == 0:
+        df = pd.DataFrame({
+            'node': l_name[i],
+            'point': l_points[i]
+        })
+    else:
+        tmp_df = pd.DataFrame({
+            'node': l_name[i],
+            'point': l_points[i]
+        })
+        df = pd.concat([df, tmp_df])
+
+print(df['point'].value_counts())
 
 fig, ax = plt.subplots()
+
+ax = sns.boxplot(x='node', y='point', order=l_name, data=df)
+
+'''
+points = tuple(l_points)
+name = tuple(l_name)
 bp = ax.boxplot(points)
+fig.show()
 ax.set_xticklabels(name)
-plt.legend(loc='upper left',fontsize=20)
-plt.grid()
-plt.xlabel('Expected Error [dB]',fontname="HGGothicM",fontsize=30)
-plt.ylabel('CDF',fontname="HGGothicM",fontsize=30)
+'''
+ax.set_xlabel('The number of nodes',fontsize=30)
+ax.set_ylabel('Expecterd Error[dB]',fontsize=30)
+#plt.legend(loc='upper left',fontsize=20)
+ax.grid()
+#plt.xlabel('Expected Error [dB]',fontname="HGGothicM",fontsize=30)
+#plt.ylabel('CDF',fontname="HGGothicM",fontsize=30)
 plt.show()
